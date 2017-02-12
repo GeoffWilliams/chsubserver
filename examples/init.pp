7bea41b0d2dd6befe9670c0cc4e337bf61b6d930
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-12 11:03:57 +0800

    added example with params

--- examples/init.pp
+++ examples/init.pp
@@ -2,3 +2,9 @@ chsubserver { "rusersd":
     proto  => "udp",
     ensure => "disabled",
 }
+
+chsubserver { "ftp":
+    proto  => "udp",
+    ensure => "enabled",
+    params => "ftpd -l -u077",
+}
